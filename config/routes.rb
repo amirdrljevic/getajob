4bc219009219b87b3510437a2e4276d5b27ce0bf
amirdrljevic <amirdrljevic@gmail.com>
2021-11-03 13:47:15 +0100

    Basic styling added (#3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  match '/users/:id',     to: 'users#show',       via: 'get'
+  devise_for :users, :path_prefix => 'd'
+  resources :users, :only =>[:show]
   root 'home#index'
 end
