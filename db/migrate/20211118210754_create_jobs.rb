77965dd65db7c52230a9cebbeaa9e5eff4aeba73
Amir Drljevic <amirdrljevic@gmail.com>
2021-11-18 22:26:20 +0100

    Created scaffolds for Category and Job

---
+++ db/migrate/20211118210754_create_jobs.rb
@@ -0,0 +1,13 @@
+class CreateJobs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.text :description
+      t.references :category, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.date :valid_until
+
+      t.timestamps
+    end
+  end
+end
